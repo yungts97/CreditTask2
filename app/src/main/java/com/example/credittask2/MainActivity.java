7efd8fdc6082e3c63b8c06640c23f1af409c030b
yungts97 <yungts97@gmail.com>
2019-11-19 15:45:42 +0800

    first commit

---
+++ app/src/main/java/com/example/credittask2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.credittask2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/example/credittask2/PagerAdapter.java
@@ -0,0 +1,46 @@
+package com.example.credittask2;
+
+import android.widget.Switch;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    private String titles[] = {"NOW SHOWING", "UPCOMING MOVIES", "WATCH LIST"};
+
+    public PagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch(position)
+        {
+            case 0:
+                return ShowingFragment.newInstance();
+            case 1:
+                return ComingFragment.newInstance();
+            case 2:
+                return WatchFragment.newInstance();
+
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+}
